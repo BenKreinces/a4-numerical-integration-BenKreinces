0340f2cc449d0cd6761ac35ff4f345fe24533b2f
Ben Kreinces <bkreince@calpoly.edu>
2026-09-16 13:32:34 -0700

    created file structure and initial integration code by just working alongisde the Appendix.

---
+++ integration.py
@@ -0,0 +1,10 @@
+from collections.abs import Callable
+def rect_left(f: Callable[[int], int], a: float, b: float, N: int):
+
+def rect_right(f, a: float, b, N):
+
+def rect_mid(f, float: a, b, N):
+
+def trapezoid(f, float: a, b, N):
+
+def simpson(f, float: a, b, N):
